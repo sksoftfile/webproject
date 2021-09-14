822f55ccfd71ce19332f221ff22a48788d5ea207
Shashank Singh <shashankkumarsingh141@gmail.com>
2021-09-14 11:32:21 +0530

    add in huroku

--- public/js/main.js
+++ public/js/main.js
@@ -1,11 +1,8 @@
 const cityName = document.getElementById('cityName');
 const submitBtn = document.getElementById('submitBtn');
-
 const city_name = document.getElementById('city_name');
-
 const temp_real_val = document.getElementById('temp_real_val');
 const temp_status = document.getElementById('temp_status');
-
 const datahide = document.querySelector('.middle_layer');
 
 
@@ -14,14 +11,15 @@ const getInfo = async(event) =>{
     let cityVal = cityName.value; 
     if(cityVal === ""){
         city_name.innerText = `Plz write the name before search`;
-        datahide.classList.add('data_hide');
+        datahid 
+        e.classList.add('data_hide');
 
     }else{
         try{
             let url = `http://api.openweathermap.org/data/2.5/weather?q=${cityVal}&units=metric&appid=b14425a6554d189a2d7dc18a8e7d7263`;
             const response = await fetch(url);
             const data = await response.json();
-           const arrData = [data];
+            const arrData = [data];
 
            city_name.innerText = `${arrData[0].name}, ${arrData[0].sys.country}`;
            temp_real_val.innerText = arrData[0].main.temp;
